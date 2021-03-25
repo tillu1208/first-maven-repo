a4630be2c63c6305b7e3f559bf12879da6504f18
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-25 12:37:18 +0530

    second user

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,8 +6,8 @@ public class EmpView {
 
 	public static void main(String[] args) {
 		Employee emp=new Employee();
-		emp.setEmpId(1001);
-		emp.setEmpName("John");
+		emp.setEmpId(1002);
+		emp.setEmpName("Mark");
 		System.out.println("Id:"+emp.getEmpId());
 		System.out.println("Name:"+emp.getEmpName());
 	}
