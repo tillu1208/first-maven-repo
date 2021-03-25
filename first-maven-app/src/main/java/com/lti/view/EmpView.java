67ea4efc0860e1e69aa2e156da75c97a2325cae7
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-25 12:50:05 +0530

    same mark commit

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -5,8 +5,10 @@ import com.lti.model.Employee;
 public class EmpView {
 
 	public static void main(String[] args) {
+		//Employee emp= new Employee();
+		
 		Employee emp=new Employee();
-		emp.setEmpId(1002);
+		emp.setEmpId(100);
 		emp.setEmpName("Mark");
 		System.out.println("Id:"+emp.getEmpId());
 		System.out.println("Name:"+emp.getEmpName());
